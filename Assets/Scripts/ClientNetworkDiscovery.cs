0a0550bb4cd5c642f176234830054b4c74c78495
alienity <wenxiangzuo@tencent.com>
2018-07-22 14:58:22 +0800

    miniGame客户端

---
+++ Assets/Scripts/ClientNetworkDiscovery.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class ClientNetworkDiscovery : NetworkDiscovery
+{
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        StopBroadcast();
+        string ip = "";
+        bool ok = false;
+        foreach (var c in fromAddress)
+        {
+            if (c >= '0' && c <= '9') ok = true;
+            if (ok) ip += c;
+        }
+        Debug.Log(ip);
+        Client.ipv4 = ip;
+    }
+}
